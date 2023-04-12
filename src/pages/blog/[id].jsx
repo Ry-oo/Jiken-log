7fdfe0e61ab45fbc6b327e03b58b1f95ce12bec7
ryoo <ryooc072112@gmail.com>
2023-04-12 23:48:00 +0900

    ⚡️ microcmsでの活動内容の部分のページ制作

---
+++ src/pages/blog/[id].jsx
@@ -0,0 +1,29 @@
+import { client } from "../../../libs/client";
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "jlog", contentId: id });
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "jlog" });
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+export default function Blog({ blog }) {
+  return (
+    <div className="bg-gray-50">
+      <div className="h-screen  mx-60  ">
+        <h1 className="text-2xl text-center">{blog.title}</h1>
+        <br />
+        <article dangerouslySetInnerHTML={{ __html: `${blog.body}` }} />
+      </div>
+    </div>
+  );
+}
--- src/pages/post/active.jsx
+++ src/pages/post/active.jsx
@@ -1,4 +1,39 @@
-export const Active = () => {
-  return <div>hello</div>;
+import Link from "next/link";
+import { client } from "../../../libs/client";
+import MediaCard from "../parts/card";
+
+export const getStaticProps = async () => {
+  const data = await client.get({
+    endpoint: "jlog",
+  });
+  console.log(data);
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
 };
+
+export const Active = ({ blog }) => {
+  return (
+    <div className="h-screen bg-green-50">
+      <h2 className="text-3xl text-center italic">
+        ~児童教育研究会の主な活動内容（2023編）~
+      </h2>
+
+      <div>
+        <ul className="grid grid-cols-3 gap-4 m-10">
+          {blog.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>
+                <MediaCard image={blog.image} title={blog.title} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 export default Active;
