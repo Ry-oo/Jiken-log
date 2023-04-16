d558c1bf64fd41c5bafe694ea7ddd5fbb712759d
ryoo <ryooc072112@gmail.com>
2023-04-16 21:36:00 +0900

    ⚡️ カード部分を共通化

---
+++ src/components/mediaCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+/**Microcmsをインポートしてタイトルを表示させる */
+
+export const MediaCard = ({ image, title }) => {
+  return (
+    <Card className="w-80">
+      <CardMedia className="h-32" image={image.url} title="タイトル" />
+
+      <CardContent>
+        <Typography>{title}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
